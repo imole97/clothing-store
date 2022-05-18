40e9f91146f9bfc3b23d3453ddb32fea743cc3bd
Imole97 <imoleadebanjo97@gmail.com>
2022-05-18 20:41:16 +0100

    created stripe utils, .env, stripePromise from loadStripe; wrapped App component with Elements

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
+import {Elements} from '@stripe/react-stripe-js'
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,7 +18,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor = {persistor}>
         <BrowserRouter>
-                <App />
+          <Elements stripe={stripePromise}>
+              <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
